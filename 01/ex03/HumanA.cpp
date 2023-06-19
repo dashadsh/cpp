ed52262c4b88fc5fc6434c6c8473770795ab98fa
Daria Goremykina <d.goremykina@gmail.com>
2023-06-19 12:24:12 +0200

    m

--- 01/ex03/HumanA.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include "HumanA.hpp"
-
-HumanA::HumanA(const std::string& name, const Weapon& weapon) : name(name), weapon(weapon) {}
-
-void HumanA::attack() const {
-    std::cout << name << " attacks with their " << weapon.getType() << std::endl;
-}
