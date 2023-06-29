a0871655a1115b5a5f3cdd04fd9786d27788678a
Daria Goremykina <d.goremykina@gmail.com>
2023-06-29 14:48:28 +0200

    m

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -1,17 +1,22 @@
 #include "Fixed.hpp"
 #include <iostream>
 
-// Default constructor - initializes the fixed-point number value to 0.
+// default constructor - init. fixed-point number value to 0
 Fixed::Fixed() : value(0) {
     std::cout << "Default constructor called" << std::endl;
 }
 
-// Copy constructor - creates a new instance that is a copy of the source instance.
+// copy constructor - creates new instance that is a copy of the source instance
 Fixed::Fixed(const Fixed &source) : value(source.value) {
     std::cout << "Copy constructor called" << std::endl;
 }
 
-// Copy assignment operator - assigns the source instance's values to the current instance.
+// destructor
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+// copy assignment operator - assigns the source instance's values to the current instance
 Fixed& Fixed::operator=(const Fixed &source) {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &source)
@@ -19,18 +24,13 @@ Fixed& Fixed::operator=(const Fixed &source) {
     return *this; // Return a reference to enable chain assignment
 }
 
-// Destructor
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
-}
-
-// Returns the raw value
+// returns the raw value
 int Fixed::getRawBits() const {
     std::cout << "getRawBits member function called" << std::endl;
     return value;
 }
 
-// Sets the raw value
+// sets the raw value
 void Fixed::setRawBits(int const raw) {
     value = raw;
 }
--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -3,17 +3,17 @@
 
 class Fixed {
 public:
-    Fixed(); // Default constructor
-    Fixed(const Fixed &source); // Copy constructor
-    Fixed& operator=(const Fixed &source); // Copy assignment operator
-    ~Fixed(); // Destructor
+    Fixed(); // default constructor
+    Fixed(const Fixed &source); // copy constructor
+	~Fixed(); // destructor
 
-    int getRawBits() const; // Returns the raw value
-    void setRawBits(int const raw); // Sets the raw value
+    Fixed& operator=(const Fixed &source); // copy assignment operator
+    int getRawBits() const; // returns raw value
+    void setRawBits(int const raw); // sets raw value
 
 private:
-    int value; // Fixed-point number value
-    static const int fractionalBits = 8; // Number of fractional bits
+    int value; // fixed-point number value
+    static const int fractionalBits = 8; // number of fractional bits
 };
 
-#endif // FIXED_HPP
+#endif
