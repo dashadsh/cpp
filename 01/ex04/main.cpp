c04543f4e535ea140b3885f749499a57d0b30587
Daria Goremykina <d.goremykina@gmail.com>
2023-06-30 16:17:38 +0200

    m

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -5,60 +5,160 @@
 // writes the result to a new file
 // ------------------------------------------------------------------------------------------------------------
 
-#include <iostream>  // std::cerr, std::endl
-#include <fstream>  // std::ifstream, std::ofstream
-#include <sstream>  // std::istringstream
+// In 2nd implementation each line is splitted into words and each word is checked against s1. 
+// if a match is found,the word will be replaced with s2. 
+// This approach will work well if you're trying to match and replace entire words. 
+// If s1 is a substring that's part of a larger word, it won't be replaced 
+// because it won't match any of the words extracted from the line.
 
-void replace(std::string filename, std::string s1, std::string s2) {
-//void replace(const std::string &filename, const std::string &s1, const std::string &s2) { - BETTER, SEE BELOW-1
-    std::ifstream input_file(filename); // read from a file
+// 1st implementation doesn't split the lines into words. 
+// Instead, it checks the entire line for instances of s1, 
+// regardless of whether s1 is an entire word or part of a word. 
+// If a match is found, it's replaced with s2.
 
-    if (!input_file) { // check if the inputfile was successfully opened
-        std::cerr << "error opening input file: " << filename << std::endl;
-        return;
-    }
+// ------------------------------------------------------------------------------------------------------------
 
-    std::ofstream output_file(filename + ".replace"); // write to a file
+#include <fstream>  // file input/output
+#include <iostream> // input/output on the console
+#include <string>   // for string data type
 
-    if (!output_file) { // check if outfile was successfully opened
-        std::cerr << "error opening output file: " << filename << ".replace" << std::endl;
-        return;
-    }
+int main(int ac, char *av[]) {
 
-    std::string line; // holds one line from the file which we will read
-
-    while (std::getline(input_file, line)) {  // read the file line by line using std::getline
-        std::istringstream iss(line);  // std::istringstream - split the line into words
-        std::string word; // placeholder for a single word
-        bool isFirstWord = true;
-
-        while (iss >> word) { // read the words from the line 'iss'
-            if (!isFirstWord) {
-                output_file << ' '; // print a space after 1st word/between words, SEE BELOW-2
-            }
-
-            if (word == s1) { // if current word is equal to the 1st input string, replace it
-                output_file << s2;
-            } else { // if not, print word as it is
-                output_file << word;
-            }
-
-            isFirstWord = false;
-        }
-
-        output_file << '\n'; //prints newline after each line
-    }
-}
-
-int main(int ac, char **av) {
     if (ac != 4) {
-        std::cerr << "usage: " << av[0] << " filename s1 s2" << std::endl;
+        std::cerr << "usage: ./program <filename> <string1> <string2>\n";
         return 1;
     }
-    replace(av[1], av[2], av[3]);
+
+    // try to open the input file. std::ifstream is an input file stream which can read from files
+    std::ifstream inFile(av[1]);
+    // try to open the output file. std::ofstream is an output file stream which can write to files
+    std::ofstream outFile(std::string(av[1]) + ".replace");
+
+    if (!inFile) {
+        std::cerr << "Error opening input file\n";
+        return 1;
+    }
+
+    if (!outFile) {
+        std::cerr << "Error opening output file\n";
+        return 1;
+    }
+
+    // create string obj's to store lines from file, and 2 strings to find and replace
+    std::string line;
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+
+    // while there are lines to read from input file:
+    while (std::getline(inFile, line)) {
+        // keep replacing s1 with s2 in current line
+        std::size_t pos;
+		// this loop will find and process each occurrence of s1 in line, 
+		// stopping when there are no more occurrences of s1 left:
+        while ((pos = line.find(s1)) != std::string::npos) {
+		// loop continues as long as s1 is found within line. 
+		// It stops when s1 is no longer found, i.e., when line.find(s1) returns std::string::npos.
+            line.erase(pos, s1.length());  // erase s1 from the line
+            line.insert(pos, s2);  // insert s2 in its place
+        }
+
+        outFile << line << '\n';	// write modified line to the output file
+    }
+
+	// std::ifstream and std::ofstream objects automatically close the file they're 
+	// associated with when they are destructed,
+	// calls to close are not technically necessary but can be considered good practice 
+	// because they make the code's intention clear
+    inFile.close();
+    outFile.close();
 
     return 0;
 }
+
+// For the dot (.) in line.insert(pos, s2) etc -  it's used to call the insert member function 
+// on the std::string object line.
+
+// In C++, the dot operator (.) is used to access members (functions or variables) of an object. 
+// For example, if you have an object obj of a class MyClass, 
+// and MyClass has a member function func, you could call func on obj like this: obj.func().
+
+// The functions that are "just used as they are" are free functions that aren't associated with a specific class. 
+// For example, std::getline is a free function that can read a line from a stream. 
+// You can use it on any stream, so it doesn't belong to any specific stream object.
+
+// On the other hand, functions used with the dot operator, like insert, are member functions. 
+// They belong to a specific class and operate on objects of that class. 
+// The insert function is a member of the std::string class and operates on std::string objects. 
+// It can't be used on its own; it needs a std::string object to work on.
+
+// ------------------------------------------------------------------------------------------------------------
+
+// #include <iostream>  // std::cerr, std::endl
+// #include <fstream>  // std::ifstream, std::ofstream
+// #include <sstream>  // std::istringstream
+
+// Open FD's: 
+// The std::ifstream and std::ofstream objects (input_file and output_file) will automatically close 
+// their associated files when they are destroyed. 
+// They are local variables in the replace function, so they are destroyed when the function returns,
+// which means the files they are associated with are closed.
+
+// void replace(std::string filename, std::string s1, std::string s2) {
+// //	void replace(const std::string &filename, const std::string &s1, const std::string &s2) { - BETTER, SEE BELOW-1
+// //	std::ifstream input_file(filename); // read from a file
+// 	std::ifstream input_file(filename.c_str()); // works with C++98
+// //  In older versions of C++ the std::ifstream and std::ofstream constructors only accept const char*
+
+
+//     if (!input_file) { // check if the inputfile was successfully opened
+//         std::cerr << "error opening input file: " << filename << std::endl;
+//         return;
+//     }
+
+// //	std::ofstream output_file(filename + ".replace"); // write to a file
+// 	std::ofstream output_file((filename + ".replace").c_str());
+// //	In older versions of C++ the std::ifstream and std::ofstream constructors only accept const char*
+
+
+//     if (!output_file) { // check if outfile was successfully opened
+//         std::cerr << "error opening output file: " << filename << ".replace" << std::endl;
+//         return;
+//     }
+
+//     std::string line; // holds one line from the file which we will read
+
+//     while (std::getline(input_file, line)) {  // read the file line by line using std::getline
+//         std::istringstream iss(line);  // std::istringstream - split the line into words
+//         std::string word; // placeholder for a single word
+//         bool isFirstWord = true;
+
+//         while (iss >> word) { // read the words from the line 'iss'
+//             if (!isFirstWord) {
+//                 output_file << ' '; // print a space after 1st word/between words, SEE BELOW-2
+//             }
+
+//             if (word == s1) { // if current word is equal to the 1st input string, replace it
+//                 output_file << s2;
+//             } else { // if not, print word as it is
+//                 output_file << word;
+//             }
+
+//             isFirstWord = false;
+//         }
+
+//         output_file << '\n'; //prints newline after each line
+//     }
+// }
+
+// int main(int ac, char **av) {
+//     if (ac != 4) {
+//         std::cerr << "usage: " << av[0] << " filename s1 s2" << std::endl;
+//         return 1;
+//     }
+//     replace(av[1], av[2], av[3]);
+
+//     return 0;
+// }
 
 // -----------------------------------------------------------------------------------
 // Efficiency: 
--- 02/ex00/main.cpp
+++ 02/ex00/main.cpp
@@ -1,3 +1,16 @@
+
+/* ---------------------------------------------------------------------------
+From now on, all your classes must be designed in the Orthodox Canonical Form, 
+unless explicitely stated otherwise. 
+Then, they will implement the four required member functions below:
+• Default constructor
+• Copy constructor
+• Copy assignment operator • Destructor
+Split your class code into two files. 
+
+The header file (.hpp/.h) contains the class definition 
+whereas the source file (.cpp) contains the implementation.
+-----------------------------------------------------------------------------*/
 #include "Fixed.hpp"
 #include <iostream>
 
